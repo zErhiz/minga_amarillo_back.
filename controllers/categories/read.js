9d047b1c1a160e3c2d811a651246b9b16df8953b
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-04-24 12:03:57 -0300

    b-09

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,5 +1,15 @@
-let read = (req, res, next) => res.status(200).render('index', {
-    title: '/categories',
-    subtitle: 'endpoint of categories'
-  })
+import Category from "../../models/Category.js"
+let read = async(req, res, next) => { // la funcion controladora debe ser asincrona para poder esperar la respuesta de mongo 
+    try                             //utilizo try cath para intentar algo y agarrar los errores q puedan surgir 
+    {
+let all = await Category.find()       //utilizo find para buscaR todos los recursos del modelo que es category
+return res.status(200)                   //configuro la respuesta que le tengo que enviar al cliente(front)
+.json({
+        categories: all
+    })                            
+    }
+    catch(error){}
+ console.log(error)
+ return res.status(400).json({error: "a ocurrido un problema"})
+  }
   export default read
