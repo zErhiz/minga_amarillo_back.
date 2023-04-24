edaabbbcf54a9b427a80c3c3afe929c22df3363d
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-04-24 13:25:33 -0300

    main

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//middlewares
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
