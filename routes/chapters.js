4930548cb855da193824295a9c0792bf298326d4
unknown <cris.sandoval.cf@gmail.com>
2023-05-01 13:06:41 -0300

    passport

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,12 +1,13 @@
 import  { Router }  from 'express';
 import read from '../controllers/chapters/read.js'
 import create from '../controllers/chapters/create.js';
-import validator from "../middlewares/validator.js"
+import validator from "../middlewares-M04/validator.js"
 import chapterCreate from "../schemas/chapters.js"
+import accountExistsSignUp from '../middlewares-M04/accontSingup.js';
 
 let router = Router()
 
 router.get('/', read);
-router.post('/',validator(chapterCreate) ,create)
+router.post('/',validator(chapterCreate), accountExistsSignUp ,create)
   
 export default router
