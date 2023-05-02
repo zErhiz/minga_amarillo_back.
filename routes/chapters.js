21d9e9a3c6c7cf86acdda5505ad593ccfcb20bb6
Cristian Sandoval <123500579+cris-sandoval@users.noreply.github.com>
2023-05-02 16:58:13 -0300

    M 04 (#10)
    * chapter
    
    * passport
    
    * back-m-04
    
    * back-ok

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,7 +1,15 @@
 import  { Router }  from 'express';
 import read from '../controllers/chapters/read.js'
+import create from '../controllers/chapters/create.js';
+import validator from "../middlewares-M04/validator.js"
+import chapterCreate from "../schemas/chapters.js"
+import passport from '../middlewares-M04/passport.js';
+
 let router = Router()
 
 router.get('/', read);
+router.post('/', passport.authenticate('jwt',{session:false}),validator(chapterCreate), create)
   
 export default router
+
+// 
