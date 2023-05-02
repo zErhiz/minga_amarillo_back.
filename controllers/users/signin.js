9b08b2ec6a50fc3f6c01b5499329c45aa9790e2a
lucas271102 <lucas001spe@gmail.com>
2023-05-02 18:37:28 -0300

    fixes

--- controllers/users/signin.js
+++ controllers/users/signin.js
@@ -1,4 +1,5 @@
 import User from '../../models/User.js'
+import  jwt  from 'jsonwebtoken'
 let signin = async(req, res, next)=>{
   try{
     
@@ -7,9 +8,23 @@ let signin = async(req, res, next)=>{
     {is_online:true},
     
    )
+   const token = jwt.sign(
+    {id:req.user._id},
+    process.env.TOKEN,
+    {expiresIn:60*60*24}
+   )
+   const user = {
+    email:req.user.email,
+    photo:req.user.photo,
+    role:req.user.role
+  } 
+   user.password = null
    return res.status(200).json({
     success:true,
-    message:'user signed in!'
+    message:'user signed in!',
+    token,
+    user
+
    })
   }catch(error){
     next(error)
