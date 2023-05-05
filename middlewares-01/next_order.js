857bc9dadf1feb59d265fcb546a7d031a5aa5d60
lucas271102 <lucas001spe@gmail.com>
2023-05-05 18:21:22 -0300

    mdl-0106FIX

--- middlewares-01/next_order.js
+++ middlewares-01/next_order.js
@@ -3,20 +3,26 @@ import Manga from '../models/Manga.js'
 
 
 async function next_order(req, res, next){
+    console.log('str')
+    const manga = req.body
+    console.log(manga)
     if(req.body.order){
         return next()
     }
-    const manga = req.body.manga
+   
 
     
-    if(!req.body.order){
-        const chapter=await Chapter.findOne({manga_id : manga}).sort({order : '- 1'})
-        req.body.order= chapter
+    if(req.body.order){
         
-        
+        const chapter=await Chapter.find().sort({order : '- 1'}).limit(1)
+
+        let nextOrder= chapter[0].order + 1
+        req.body.order= nextOrder
+        console.log(chapter)
+
     }
         
-       
+       return next()
 
 }
 export default next_order
