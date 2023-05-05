3e7122cc22ac3d5b4df44b6ee2832bc60d0cdfae
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-05-05 09:55:10 -0300

    middlewares m||02 && m||05 =)

---
+++ middlewares-02/is_active.js
@@ -0,0 +1,40 @@
+import Author from "../models/Author";
+import Company from "../models/Company"
+
+
+
+function is_active(req, res, next) {
+    const userId = req.user.id; // obtener el ID del usuario logueado
+    // buscar el autor correspondiente al usuario
+    Author.findOne({ user_id: userId })
+      .then((author) => {
+        if (!author) {
+          return res.status(404).json({ message: 'Author not found' });
+        }
+        if (!author.active) {
+          return res.status(401).json({ message: 'the Author is inactive' });
+        }
+       
+        Company.findOne({ user_id: userId })
+          .then((company) => {
+            if (!company) {
+              return res.status(404).json({ message: 'Company not found' });
+            }
+            if (!company.active) {
+              return res.status(401).json({ message: 'Coumpany inactive' });
+            }
+            // si el autor y la companie esta activo y existe continuar con la siguiente función
+            next();
+          })
+          .catch((error) => {
+            console.error(error);
+            return res.status(500).json({ message: 'Error' });
+          });
+      })
+      .catch((error) => {                                        //El Error 500 significa que algo ha fallado en el sitio web.
+        console.error(error);
+        return res.status(500).json({ message: 'Error ' });
+      });
+  }
+  
+  export default is_active;
