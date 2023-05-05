224a3bff51770b475ecbb84d1826f0020f17b12d
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-05-05 12:43:30 -0300

    fixed

--- middlewares-02/is_active.js
+++ middlewares-02/is_active.js
@@ -6,27 +6,27 @@ import Company from "../models/Company.js"
 async function is_active(req, res, next) {
   const userId = req.user.id;
   
-  Author.findOne({ user_id: userId })
-    .then((author) => {
-      if (!author) {
-        return res.status(404).json({ message: 'Author not found' });
-      }
-      if (!author.active) {
-        return res.status(401).json({ message: 'the Author is inactive' });
-      }
+  try {
+    const author = await Author.findOne({ user_id: userId });
+    if (!author) {
+      return res.status(404).json({ message: 'Author not found' });
+    }
+    if (!author.active) {
+      return res.status(401).json({ message: 'the Author is inactive' });
+    }
 
-      Company.findOne({ user_id: userId })
-        .then((company) => {
-          if (!company) {
-            return res.status(404).json({ message: 'Company not found' });
-          }
-          if (!company.active) {
-            return res.status(401).json({ message: 'Coumpany inactive' });
-          }
-          next();
-        });
-    });
+    const company = await Company.findOne({ user_id: userId });
+    if (!company) {
+      return res.status(404).json({ message: 'Company not found' });
+    }
+    if (!company.active) {
+      return res.status(401).json({ message: 'Company inactive' });
+    }
+
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: 'error' });
+  }
 }
-
   
   export default is_active;
