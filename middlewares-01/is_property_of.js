68ae5b998952982fda0936388811f71438bd659e
FranciscoPedicino <127889331+FranciscoPedicino@users.noreply.github.com>
2023-05-05 13:01:59 -0300

    m/03 (#17)
    * m/03
    
    * fixed

---
+++ middlewares-01/is_property_of.js
@@ -0,0 +1,19 @@
+import Manga from "../models/Manga.js";
+
+
+const property =async (req,res,next)=>{
+    const mangaId=req.body.manga_id
+    const authorId=req.body.author_id
+    try{
+const manga= await Manga.findOne( {_id:mangaId,author_id:authorId} )
+if(!manga){
+    return res.status(404).json({message:"Manga not found or not owned by author"})
+}
+next()
+    }
+    catch (err){
+return res.status(500).json({menssage:"Server error"})
+    }
+}
+
+export default property
