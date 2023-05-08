494f59ab38cfc05b2bcf191f6cde72cf8548d7d1
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-05-07 21:53:35 -0300

    GET: api/authors/:id para ver los datos del autor

--- routes/authors.js
+++ routes/authors.js
@@ -6,13 +6,14 @@ import authorSchema from '../schemas/author.js'
 import validator from '../middlewares-02/validator.js'
 import authorAlreadeExist from '../middlewares-02/authorAlreadeExist.js';
 import passport from '../middlewares-02/passport.js';
-
+import get_one from '../controllers/authors/get_one.js';
 const create = controller.create
 let router = Router()
 
 
 /* router.post('/',(req,res,next)=> res.status(200).send('autor creado')) */
 router.get('/',read)
+router.get('/:author_id',get_one)
 router.post('/',passport.authenticate('jwt',{session:false}),validator(authorSchema),authorAlreadeExist,create)
 /* router.put('/:id',(req,res,next)=> res.status(200).send('autor modificado')) */
 /* router.delete('/:id',(req,res,next)=> res.status(200).send('autor eliminado')) */
