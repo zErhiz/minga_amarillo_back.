bb94458cfad12b1aa56afa66e8ed1e81c69578b0
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-05-08 15:23:53 -0300

    fix

--- controllers/authors/get_one.js
+++ controllers/authors/get_one.js
@@ -5,7 +5,7 @@ import Author from "../../models/Author.js";
 let get_one = async(req,res,next)=>{
    try {
     let {id} = req.params
-     let all = await  Author.findById(id,'name city country photo date -_id')
+     let all = await  Author.findById(id,'name last_name city country photo date -_id')
      return res.status(200).json({
         succes: true,
         response:all,
