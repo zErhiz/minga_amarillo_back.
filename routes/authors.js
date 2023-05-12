c1fae2963645550cfddfbcfaf6a13bbb10d90831
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-05-12 10:47:48 -0300

    fix

--- routes/authors.js
+++ routes/authors.js
@@ -13,7 +13,7 @@ let router = Router()
 
 /* router.post('/',(req,res,next)=> res.status(200).send('autor creado')) */
 router.get('/',read)
-router.get('/:id',get_one)
+router.get('/:id',passport.authenticate('jwt',{session:false}),get_one)
 router.post('/',passport.authenticate('jwt',{session:false}),validator(authorSchema),authorAlreadeExist,create)
 /* router.put('/:id',(req,res,next)=> res.status(200).send('autor modificado')) */
 /* router.delete('/:id',(req,res,next)=> res.status(200).send('autor eliminado')) */
