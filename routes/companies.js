9b0ce806efcfe2ccf4a2cdadff04ebc71b11ff88
Mateo Carrizo FLores <127887470+zErhiz@users.noreply.github.com>
2023-05-15 17:07:17 -0300

    m-12back (#28)
    * fix
    
    * m-12 back
    
    * fix
    
    * fix

--- routes/companies.js
+++ routes/companies.js
@@ -4,11 +4,11 @@ import passport from '../middlewares-M04/passport.js';
 import companyCreate from "../schemas/companies.js"
 import create from '../controllers/companies/create.js';
 import validator from '../middlewares-M04/validator.js';
-
+import admin from '../controllers/companies/admin.js';
 let router = Router()
 
-router.get('/', read);
-
+router.get('/', passport.authenticate('jwt',{session:false}),read);
+router.get('/admin',passport.authenticate('jwt',{session:false}),admin)
 router.post('/',passport.authenticate('jwt',{session:false}), validator(companyCreate), create)  
 export default router
 
